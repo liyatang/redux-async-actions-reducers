89f7b2ef1f99d751b99b336ebd215aaf250224ad
liyatang <liyatang@xiaonongnv.com>
2016-05-20 14:11:28 +0800

    new

--- src/reducers.js
+++ src/reducers.js
@@ -2,30 +2,27 @@ import {combineReducers} from 'redux';
 
 let _reducers = {};
 
-/*
- * 顶层store,且要求store要很清楚是什么和一开始的时候就定义好初始状态.(这普遍吧?)
- * reducers能够通过combineReducers进行拆分.没有明显按需加载方案.
- * 且combineReducers会map一遍，也需要定义好初始状态。
+/**
+ * reducers能够通过combineReducers进行拆分，且combineReducers会map一遍，没有明显按需加载方案。
  *
- * so reducers本身是function,很容易扩展.
- * 加多一层中间层,初始返回state的初始状态(一开始redux会run一遍),如果reducers通过mapReducers按需加载进来,则切换过去.
+ * reducers本身是function，很容易扩展。
+ * 加多一层中间层，初始返回state的初始状态(一开始redux会run一遍),如果reducers通过mapReducers按需加载进来,则切换过去.
  *
  * 约定一个初始值，协助做初始化。
- * 
- * @param {function}
  *
- * @returns combineReducers
+ * @param {Function} reducers
+ * @returns {Function} combineReducers
  * */
 let combineAsyncReducers = (reducers) => {
     _reducers = reducers;
 
     let o = {};
     Object.keys(_reducers).forEach((key) => {
-        if (typeof _reducers[key] === 'function') {
+        if (toString.call(_reducers[key]) === '[object Function]') {
             o[key] = _reducers[key];
         } else {
             o[key] = (state, action) => {
-                if (typeof _reducers[key] === 'function') {
+                if (toString.call(_reducers[key]) === '[object Function]') {
                     return _reducers[key].call(null, state, action);
                 }
                 return state || _reducers[key];
